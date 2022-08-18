e30a5dc4c1e3e44110456cb8286de7d95baad3dc
Carlos <cjcarrillo@veracruz.gob.mx>
2022-08-18 10:59:27 -0500

    background

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import {Header} from "./components/Header";
 import {Footer} from "./components/Footer";
 import {Body} from "./components/Body";
+import background from "./styles/bg.svg";
+
+
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-
+    <div className={'back'}>
+        <Header/>
         <Body/>
-
-      <div className={'row'} style={{justifyContent:'center', alignContent:'center'}}>
+        <div className={'row'} style={{justifyContent:'center', alignContent:'center'}}>
           <Footer/>
-      </div>
+        </div>
     </div>
   );
 }
